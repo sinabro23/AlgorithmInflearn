2af3d56d65296e3ee7902e54a6ea601910c26b71
jinabro <kmy4869@naver.com>
2022-06-02 10:34:42 +0900

    ~11

--- main.cpp
+++ main.cpp
@@ -3,46 +3,25 @@
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int digit_sum(int x)
-{
-	int ret = 0;
-
-	while (x != 0)
-	{
-		ret += x % 10;
-		x /= 10;
-	}
-
-	return ret;
-}
-
 int main(int argc, char** argv) 
 {
 	//freopen("input.txt", "rt", stdin);
-	int N, num = 0, max = -2147000000, sum = 0, maxNum = 0;
+	int N = 0;
 	cin >> N;
 
-	for (int i = 0; i < N; i++)
+	int num = 0, sum = 0;
+
+	for (int i = 1; i <= N; i++)
 	{
-		cin >> num;
-
-		if (digit_sum(num) > max)
+		num = i;
+		while (num >0)
 		{
-			max = digit_sum(num);
-			maxNum = num;
+			sum++;
+			num /= 10;
 		}
-
-		else if(digit_sum(num) == max)
-		{
-			if (num > maxNum)
-			{
-				maxNum = num;
-			}
-		}
-
 	}
 
-	cout << maxNum;
+	cout << sum;
 
 	return 0;
 }
