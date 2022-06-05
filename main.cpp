aa2295053dfbafaa8735c5ac422136e169209b74
jinabro <kmy4869@naver.com>
2022-06-05 15:12:26 +0900

    ~14

--- main.cpp
+++ main.cpp
@@ -3,33 +3,48 @@
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int reverse(int x)
+{
+	int res = 0, temp = 0;
+	while (x > 0)
+	{
+		temp = x % 10;
+		res = res * 10 + temp;
+		x /= 10;
+	}
+
+	return res;
+}
+
+bool isPrime(int x)
+{
+	if (x == 1)
+		return false;
+	
+	for (int i = 2; i < x; i++)
+	{
+		if (x % i == 0)
+			return false;
+	}
+
+	return true;
+}
+
+
 int main(int argc, char** argv) 
 {
 	//freopen("input.txt", "rt", stdin);
-	string str;
-	cin >> str;
+	int n = 0;
+	cin >> n;
 
-	int cnt[10] = { 0 };
-	int max = -2147000000, maxIndex = -1;
-
-	for (int i = 0; i < str.size(); i++)
+	int num = 0;
+	for (int i = 0; i < n; i++)
 	{
-		int index = str[i] - '0';
-		cnt[index]++;
+		cin >> num;
+		num = reverse(num);
+		if (isPrime(num))
+			cout << num << ' ';
 	}
-
-	for (int i = 0; i <= 9; i++)
-	{
-		if (cnt[i] >= max)
-		{
-			max = cnt[i];
-			maxIndex = i;
-		}
-	}
-
-	cout << maxIndex;
-	
-
 
 	return 0;
 }
