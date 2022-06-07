28faa8f77fc78b19e996daa8f1509d022c823e5b
jinabro <kmy4869@naver.com>
2022-06-07 12:25:01 +0900

    ~16 아나그램

--- main.cpp
+++ main.cpp
@@ -3,48 +3,51 @@
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int reverse(int x)
-{
-	int res = 0, temp = 0;
-	while (x > 0)
-	{
-		temp = x % 10;
-		res = res * 10 + temp;
-		x /= 10;
-	}
-
-	return res;
-}
-
-bool isPrime(int x)
-{
-	if (x == 1)
-		return false;
-	
-	for (int i = 2; i < x; i++)
-	{
-		if (x % i == 0)
-			return false;
-	}
-
-	return true;
-}
-
 
 int main(int argc, char** argv) 
 {
 	//freopen("input.txt", "rt", stdin);
-	int n = 0;
-	cin >> n;
+string str;
+	cin >> str;
 
-	int num = 0;
-	for (int i = 0; i < n; i++)
+	int a[70] = { 0 };
+	int b[70] = { 0 };
+
+	for (int i = 0; i < str.size(); i++)
 	{
-		cin >> num;
-		num = reverse(num);
-		if (isPrime(num))
-			cout << num << ' ';
+		if (str[i] >= 65 && str[i] <= 90)
+		{
+			a[str[i] - 64]++;
+		}
+		else
+		{
+			a[str[i] - 96]++;
+		}
 	}
+
+	cin >> str;
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (str[i] >= 65 && str[i] <= 90)
+		{
+			b[str[i] - 64]++;
+		}
+		else
+		{
+			b[str[i] - 96]++;
+		}
+	}
+
+	for (int i = 1; i <= 52; i++)
+	{
+		if (a[i] != b[i])
+		{
+			cout << "NO";
+			exit(0);
+		}
+	}
+
+	cout << "YES";
 
 	return 0;
 }
