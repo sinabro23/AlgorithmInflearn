9c006f96d08e2f118069f1f237b5de46291f0bd1
jinabro <kmy4869@naver.com>
2022-06-13 13:17:27 +0900

    ~21

--- main.cpp
+++ main.cpp
@@ -7,42 +7,56 @@ using namespace std;
 int main(int argc, char** argv) 
 {
 	//freopen("input.txt", "rt", stdin);
-int n = 0;
-	cin >> n;
+int totalA = 0, totalB = 0;
+	int lastWinner = 0; // 1¿Ã∏È A, 2∏È B
 
-	int* A = new int[n];
-	int* B = new int[n];
-
-	for (int i = 0; i < n; i++)
+	int A[10] = { 0 };
+	int B[10] = { 0 };
+	for (int i = 0; i < 10; i++)
 	{
 		cin >> A[i];
 	}
 
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < 10; i++)
 	{
 		cin >> B[i];
 	}
 
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < 10; i++)
 	{
 		if (A[i] == B[i])
-			cout << 'D' << endl;
-		else if (A[i] == 1 && B[i] == 2)
-			cout << "B" << endl;
-		else if (A[i] == 1 && B[i] == 3)
-			cout << "A" << endl;
-		else if (A[i] == 2 && B[i] == 1)
-			cout << "A" << endl;
-		else if (A[i] == 2 && B[i] == 3)
-			cout << "B" << endl;
-		else if (A[i] == 3 && B[i] == 1)
-			cout << "B" << endl;
-		else if (A[i] == 3 && B[i] == 2)
-			cout << "A" << endl;
+		{
+			totalA += 1;
+			totalB += 1;
+		}
+		else if (A[i] > B[i])
+		{
+			totalA += 3;
+			lastWinner = 1;
+		}
+		else
+		{
+			totalB += 3;
+			lastWinner = 2;
+		}
 	}
 
-//df
-	delete[] A;
-	delete[] B;
+	cout << totalA << ' ' << totalB << endl;
+
+	if (totalA > totalB)
+		cout << "A";
+	else if (totalB > totalA)
+		cout << "B";
+	else
+	{
+		if (lastWinner == 1)
+			cout << "A";
+		else if (lastWinner == 2)
+			cout << "B";
+		else if (lastWinner == 0)
+			cout << "D";
+	}
+
+
 	return 0;
 }
