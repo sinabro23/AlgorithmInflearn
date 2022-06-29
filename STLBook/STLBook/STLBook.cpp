9e1c9db856126c2a7ba694f8e7235b676aee3990
jinabro <kmy4869@naver.com>
2022-06-29 18:42:22 +0900

    STL 알고리즘

--- STLBook/STLBook/STLBook.cpp
+++ STLBook/STLBook/STLBook.cpp
@@ -3,30 +3,34 @@
 #include <algorithm>
 using namespace std;
 
+bool Pred(int n)
+{
+    return 35 < n;
+}
+
 int main()
 {
     vector<int> v;
+    
     v.push_back(10);
     v.push_back(20);
     v.push_back(30);
-    v.push_back(30);
-    v.push_back(40);
     v.push_back(40);
     v.push_back(50);
 
-    for (auto i = 0; i < v.size(); i++)
-        cout << v[i] << " ";
-    cout << endl;
-
     vector<int>::iterator iter;
-    iter = adjacent_find(v.begin(), v.end());
-
+    iter = find(v.begin(), v.end(), 20);
     if (iter != v.end())
-        cout << *iter << " ";
-    cout << endl;
-   
+        cout << *iter << "을(를) 찾았습니다" << endl;
 
-    return 0;
+    iter = find_if(v.begin(), v.end(), Pred);
+    if (iter != v.end())
+        cout << "35 보다 큰 첫 번째 원소 : " << *iter << endl;
+ 
+
+
+
+ 
         
 
 }
