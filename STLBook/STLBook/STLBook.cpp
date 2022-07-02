75aadd53ef040cff2014f6cce687019150f264fe
jinabro <kmy4869@naver.com>
2022-07-02 23:02:52 +0900

    vector 다시

--- STLBook/STLBook/STLBook.cpp
+++ STLBook/STLBook/STLBook.cpp
@@ -10,23 +10,33 @@ bool Pred(int n)
 
 int main()
 {
-    vector<int> v;
+    vector<int> v1;
     
-    v.push_back(10);
-    v.push_back(20);
-    v.push_back(30);
-    v.push_back(40);
-    v.push_back(50);
+    v1.push_back(10);
+    v1.push_back(20);
+    v1.push_back(30);
+    v1.push_back(40);
+    v1.push_back(50);
+    v1.push_back(60);
+    v1.push_back(70);
+    v1.push_back(30);
+    v1.push_back(40);
+    v1.push_back(50);
+    v1.push_back(60);
+
+    vector<int> v2;
+    v2.push_back(30);
+    v2.push_back(40);
+    v2.push_back(50);
 
     vector<int>::iterator iter;
-    iter = find(v.begin(), v.end(), 20);
-    if (iter != v.end())
-        cout << *iter << "을(를) 찾았습니다" << endl;
-
-    iter = find_if(v.begin(), v.end(), Pred);
-    if (iter != v.end())
-        cout << "35 보다 큰 첫 번째 원소 : " << *iter << endl;
- 
+    iter = find_end(v1.begin(), v1.end(), v2.begin(), v2.end());
+    if (iter != v1.end())
+    {
+        cout << "iter : " << *iter << endl;
+        cout << "iter-1 : " << *(iter - 1) << endl;
+        cout << "iter+1 : " << *(iter + 1) << endl;
+    }
 
 
 
