3a97973d517f5f66e9858133ab505b50c161addf
jinabro <kmy4869@naver.com>
2022-09-05 18:25:26 +0900

    ~숫자 총개수

--- main.cpp
+++ main.cpp
@@ -1,47 +1,13 @@
 #include<iostream>
 using namespace std;
 
-int digit_sum(int x)
-{
-	int sum = 0;
-	
-	while(x > 0)
-	{
-		sum += x % 10;
-		x /= 10;	
-	}
-	
-	return sum;
-}
-
 int main()
 {
 	//freopen("input.txt", "rt", stdin);
-	int maxSum = -2147000000, maxNum = -2147000000;
-	int cnt = 0; 
-	cin>>cnt;
+	int n;
+	cin>>n;
 	
-	for(int i = 0; i< cnt; i++)
-	{
-		int number = 0;
-		cin >> number;
-		
-		if(digit_sum(number) > maxSum)
-		{
-			maxNum = number;
-			maxSum = digit_sum(number);
-		}
-			
-		else if(digit_sum(number) == maxSum)
-		{
-			if(number > maxNum)
-				maxNum = number;
-		}
-		
-	
-	}
-	
-		cout<< maxNum;
+	int sum = 0, res = 0, cnt = 1, digit = 9;
 	
 	return 0;
 }
