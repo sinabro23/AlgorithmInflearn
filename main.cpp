e33705cb242a48106d26084645b2030ca9d993b2
jinabro <kmy4869@naver.com>
2022-09-07 17:10:35 +0900

    연속부분증가수열

--- main.cpp
+++ main.cpp
@@ -1,13 +1,36 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int main()
 {
 	//freopen("input.txt", "rt", stdin);
 	int n;
-	cin>>n;
+	cin>> n;
 	
-	int sum = 0, res = 0, cnt = 1, digit = 9;
+	int before;
+	cin >> before;
+	int number = 0, max = 1, cnt = 1;
+	for(int i = 1; i < n; i++)
+	{	
+		cin >> number;
+		if(number >= before)
+		{
+			cnt++;
+			if(cnt > max)
+			{
+				max = cnt;
+			}
+		}	
+		else
+		{
+			cnt = 1;
+		}
+		
+		before = number;
+	}
 	
-	return 0;
+	cout <<max;
+
+	return 0;	
 }
